0151483c453116de7e4aa366366bc8609c18ccfd
FiredHydra <hydra.mailhandler@gmail.com>
2020-06-05 00:52:05 -0400

    update

---
+++ src/main/java/me/firedhydra/proxyemotes/Proxyemotes.java
@@ -0,0 +1,16 @@
+package me.firedhydra.proxyemotes;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class Proxyemotes extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
